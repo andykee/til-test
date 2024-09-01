a84032bef4b5f8406f6709d9595a070e1a5ac96e
Andy Kee <andykee@gmail.com>
2024-09-01 15:47:56 -0700

    Update build.py

--- .github/scripts/build.py
+++ .github/scripts/build.py
@@ -6,3 +6,4 @@ GITHUB_WORKSPACE = os.environ['GITHUB_WORKSPACE']
 
 
 shutil.copy(os.path.join(GITHUB_WORKSPACE, '.github/theme/_config.yml'), GITHUB_WORKSPACE)
+shutil.copy(os.path.join(GITHUB_WORKSPACE, '.github/theme/index.md'), GITHUB_WORKSPACE)
